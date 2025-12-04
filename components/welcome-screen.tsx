6a0cab4e0ec70e2dd65a341346416861ee0d20c6
desbymax-ui <des.by.max@gmail.com>
2025-12-04 16:29:12 +0100

    Implement red button component

--- components/welcome-screen.tsx
+++ components/welcome-screen.tsx
@@ -53,7 +53,7 @@ export function WelcomeScreen({ onStart }: WelcomeScreenProps) {
           ))}
         </div>
 
-        <Button size="lg" onClick={onStart} className="gap-2 px-8 bg-green-600 hover:bg-green-700 text-white">
+        <Button size="lg" onClick={onStart} className="gap-2 px-8">
           Get Started
           <ArrowRight className="w-4 h-4" />
         </Button>
